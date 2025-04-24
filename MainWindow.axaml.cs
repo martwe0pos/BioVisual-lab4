718b4b7abdd9a529fffe1662e8bca934f7baf730
martwe0pos <martwe0pos@szubidu.com>
2025-04-25 00:12:40 +0200

    drugi

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,21 +1,39 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media;
 using Avalonia.Media.Imaging;
 
 namespace lab4;
 
 public partial class MainWindow : Window
 {
+    private int deg;
     public MainWindow()
     {
         InitializeComponent();
     }
-
+    
     public async void load(object sender, RoutedEventArgs e)
     {
         var openFileDialog = new OpenFileDialog();
-        var result = await openFileDialog.ShowAsync(this);
-        var bitmap = new Bitmap(result[0]);
+        var file = await openFileDialog.ShowAsync(this);
+        var bitmap = new Bitmap(file[0]);
         image.Source = bitmap;
+    }
+
+    public void degrees(object sender, RoutedEventArgs e)
+    {
+        if (sender is RadioButton rb)
+        {
+            deg = int.Parse(rb.Tag.ToString());
+        }
+    }
+    public void rotate(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button bt)
+        {
+            var rt = new RotateTransform(deg);
+            image.RenderTransform = rt;
+        }
     }
 }
