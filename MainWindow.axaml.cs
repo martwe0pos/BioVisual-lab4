bbe08e5e3836614afb818ced46b31a88d4cad51f
martwe0pos <martwe0pos@szubidu.com>
2025-04-26 00:01:19 +0200

    trzeci

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,13 +1,19 @@
+using System;
+using System.IO;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Media;
 using Avalonia.Media.Imaging;
+using AvaloniaPixelSnoop;
+using Color = Avalonia.Media.Color;
 
 namespace lab4;
 
 public partial class MainWindow : Window
 {
-    private int deg;
+    private int deg, temp_deg;
+    private WriteableBitmap bitmap;
     public MainWindow()
     {
         InitializeComponent();
@@ -17,8 +23,9 @@ public partial class MainWindow : Window
     {
         var openFileDialog = new OpenFileDialog();
         var file = await openFileDialog.ShowAsync(this);
-        var bitmap = new Bitmap(file[0]);
-        image.Source = bitmap;
+        FileStream fs = File.OpenRead(file[0]);
+        bitmap = WriteableBitmap.Decode(fs);
+        img.Source = bitmap;
     }
 
     public void degrees(object sender, RoutedEventArgs e)
@@ -32,8 +39,51 @@ public partial class MainWindow : Window
     {
         if (sender is Button bt)
         {
-            var rt = new RotateTransform(deg);
-            image.RenderTransform = rt;
+            temp_deg = deg;
+            img.RenderTransform = new RotateTransform(deg);
         }
+    }
+
+    public void invert(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button bt && bitmap != null)
+        {
+            using (BmpPixelSnoop snoop = new(bitmap))
+            {
+                for (var i = 0; i < bitmap.PixelSize.Height; i++)
+                {
+                    for (var j = 0; j < bitmap.PixelSize.Width; j++)
+                    {
+                        var orgCol = snoop.GetPixel(i, j);
+                        var invCol = Color.FromArgb(orgCol.A, (byte)(255 - orgCol.R), (byte)(255 - orgCol.G), (byte)(255 - orgCol.B));
+                        snoop.SetPixel(i ,j ,invCol);
+                    }
+                }
+                img.RenderTransform = new RotateTransform(temp_deg);
+            }
+        }
+    }
+
+    public void upside(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button bt && bitmap != null)
+        {
+            using (BmpPixelSnoop snoop = new(bitmap))
+            {
+                Color x, y;
+                for (var i = 0; i < bitmap.PixelSize.Height / 2; i++)
+                {
+                    for (var j = 0; j < bitmap.PixelSize.Width; j++)
+                    {
+                        x = snoop.GetPixel(j, i);
+                        y = snoop.GetPixel(j, bitmap.PixelSize.Height - i - 1);
+                        snoop.SetPixel(j, i, y);
+                        snoop.SetPixel(j, bitmap.PixelSize.Height - i - 1, x);
+                    }
+                }
+                img.RenderTransform = new RotateTransform(temp_deg);
+            }
+        }
+        
     }
 }
--- obj/Debug/net9.0/lab4.AssemblyInfo.cs
+++ obj/Debug/net9.0/lab4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34b58163867be440322fb46391f5df3ed626600b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+718b4b7abdd9a529fffe1662e8bca934f7baf730")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
